64ade007cc4f61dfc119d9a15c163bc5679caa4c
charandhoni97 <charandhoni97@gmail.com>
2022-08-08 15:01:31 +0000

    Adding elections

--- src/app/voting/voting.component.ts
+++ src/app/voting/voting.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { observable } from 'rxjs';
+import { Candidate } from '../Common/Candidate';
+import { Election } from '../Common/Election';
+import { CandidateService } from '../Services/candidate.service';
 
 @Component({
   selector: 'app-voting',
@@ -6,10 +11,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./voting.component.css']
 })
 export class VotingComponent implements OnInit {
-
-  constructor() { }
+  candidates:Candidate[]=[];
+ 
+  id:number=0;
+  constructor(private candidateservice:CandidateService,private route:ActivatedRoute) { }
 
   ngOnInit(): void {
-  }
+   let Id= this.route.params.subscribe(p => { this.id = p["id"]})
+    let response=this.candidateservice.GetCandidates(this.id);
+    response.subscribe();
+    
+  } 
 
 }
